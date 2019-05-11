025de1315c950dcf20bf7bac7ab1f99afeffb37f
Lauri Jalonen <laurijalonen@Angelas-MacBook-Air.local>
2019-05-11 22:18:13 +0800

    basic socket tests, added socket.io and mocha

---
+++ test/sockettest.js
@@ -0,0 +1,46 @@
+'use strict'
+
+var expect = require('chai').expect
+    , server = require('../index')
+    , io = require('socket.io-client')
+    , ioOptions = {
+        transports: ['websocket']
+        , forceNew: true
+        , reconnection: false
+    }
+    , testMsg = 'HelloWorld'
+    , sender
+    , receiver
+
+
+
+describe('Chat Events', function () {
+    beforeEach(function (done) {
+
+        // start the io server
+        server.start()
+        // connect two io clients
+        sender = io('http://localhost:3000/', ioOptions)
+        receiver = io('http://localhost:3000/', ioOptions)
+
+        // finish beforeEach setup
+        done()
+    })
+    afterEach(function (done) {
+
+        // disconnect io clients after each test
+        sender.disconnect()
+        receiver.disconnect()
+        done()
+    })
+
+    describe('Message Events', function () {
+        it('Clients should receive a message when the `message` event is emited.', function (done) {
+            sender.emit('message', testMsg)
+            receiver.on('message', function (msg) {
+                expect(msg).to.equal(testMsg)
+                done()
+            })
+        })
+    })
+})
